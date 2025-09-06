0039a0b021098fdc865e98454f452269f94cb86e
Titi Budala <titibudala@gmail.com>
2025-09-06 17:35:27 +0300

    Trying to fix websockets server in prod v9

--- apps/websockets/src/services/socketio/client.ts
+++ apps/websockets/src/services/socketio/client.ts
@@ -5,10 +5,9 @@ const io = new Server(httpServer, {
   cors: {
     origin: process.env.WEBSCOKET_CORS_ORIGIN,
     credentials: true,
-    methods: ["GET", "POST", "PUT"],
+    // methods: ["GET", "POST", "PUT"],
   },
   // allowEIO3: true,
-  // transports: ["polling", "websocket"],
   // transports: ["polling", "websocket"],
 });
 
