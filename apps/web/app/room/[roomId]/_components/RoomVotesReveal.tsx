b36051a895f303df6f4d25a72b8c86008eec73cd
Titi Budala <titibudala@gmail.com>
2025-09-09 15:57:05 +0300

    Fix linter issue

--- apps/web/app/room/[roomId]/_components/RoomVotesReveal.tsx
+++ apps/web/app/room/[roomId]/_components/RoomVotesReveal.tsx
@@ -4,7 +4,11 @@ import { useActionState, startTransition } from "react";
 import { revealVotes } from "../_actions/revealVotes";
 import { useParams } from "next/navigation";
 
-export default function ({ isDisabled }: { isDisabled?: boolean }) {
+export default function RommVotesReveal({
+  isDisabled,
+}: {
+  isDisabled?: boolean;
+}) {
   const { roomId } = useParams();
   const [_state, action, isPending] = useActionState(revealVotes, {
     success: false,
